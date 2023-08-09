f854ce1a2d7dccf04829a0ca0399417e7440a2fc
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-09 18:18:08 +0900

    refactor: 롬복 적용(컨트롤러), @Slf4j

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
     private ArticleRepository articleRepository;
@@ -20,14 +22,20 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString()); //sout 대신 log.info
+//        System.out.println(form.toString());
+
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity(); //DTO를 엔티티로 변환하려면, 엔티티 클래스를 만들어야 한다. (여기서 Article)
-        System.out.println(article.toString());
+        log.info(article.toString());
+//        System.out.println(article.toString());
+
         //Article : 엔티티 ArticleForm: DTO
+
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article); //레퍼지토리에 저장한걸 왜 반환?
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
         return "";
     }
 }
