5d8be9176befccd952f184b641b891189329cdbf
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-09 18:24:41 +0900

    refactor: 롬복 적용(컨트롤러), 로깅 기능
    @slf4j

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
     @Autowired //이걸 안하면 !!
     private MemberRepository memberRepository; // public이 아니라 private
@@ -20,14 +22,16 @@ public class MemberController {
 
     @PostMapping("/members/join")
     public String joinMembers(MemberForm form) {
+        log.info(form.toString());
 
-        System.out.println(form.toString());
         // 1. DTO -> 엔티티
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
+
         // 2. 리파지터리로 DB에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+
         return "";
     }
 
