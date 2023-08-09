7fec9a0042671291c6ab2a7bef9eae6f41bc530d
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-09 18:24:12 +0900

    refactor: 롬복 적용(엔티티)
    @AllArgsConstructor
    @ToString

--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class Member {
     @Id
     @GeneratedValue
@@ -15,18 +19,4 @@ public class Member {
     @Column
     private String password;
 
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
