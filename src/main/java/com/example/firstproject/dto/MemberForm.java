77392f54cbb82a24d7fd1a76bff27a45e2c66632
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-09 18:23:25 +0900

    refactor: 롬복 적용(DTO)
    @AllArgsConstructor
    @ToString

--- src/main/java/com/example/firstproject/dto/MemberForm.java
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -1,25 +1,16 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class MemberForm {
     private String email;
     private String password;
 
-    public MemberForm(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
     public Member toEntity() {
         return new Member(null, email, password); // DTO에서는 null로 생성해도 자동 생성된 값이 들어간다
-    }
-
-    @Override
-    public String toString() {
-        return "MemberForm{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
     }
 }
