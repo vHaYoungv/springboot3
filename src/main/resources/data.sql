a3e97716bf36fc028eef1847d9a5be80f8dcb427
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-10 21:16:59 +0900

    feat: 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUES (1,'가가','11');
+INSERT INTO article(id, title, content) VALUES (2,'나나','22');
+INSERT INTO article(id, title, content) VALUES (3,'다다','33');
