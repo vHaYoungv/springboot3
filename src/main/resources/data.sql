1f92981b5198d59ec3ad577815e3b84eb84e4fdc
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-10 23:07:38 +0900

    feat: Article 엔티티 id 자동 생성 전략 strategy = GenerationType.IDENTITY 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO article(id, title, content) VALUES (1,'가가','11');
-INSERT INTO article(id, title, content) VALUES (2,'나나','22');
-INSERT INTO article(id, title, content) VALUES (3,'다다','33');
+INSERT INTO article(title, content) VALUES ('가가','11');
+INSERT INTO article(title, content) VALUES ('나나','22');
+INSERT INTO article(title, content) VALUES ('다다','33');
 
 INSERT INTO member(id, email, password) VALUES (1,'hihi@naver.com','1111');
