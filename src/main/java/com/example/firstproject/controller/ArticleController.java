539796a48252ff3a8725247aa1a4ab4a000bbaca
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-08-10 16:51:39 +0900

    feat: Article 목록 기능 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @Slf4j
@@ -38,4 +42,32 @@ public class ArticleController {
 //        System.out.println(saved.toString());
         return "";
     }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) { //PathVariable: URL 요청으로 들어온 전달값을 의미
+        log.info("id = " + id);
+
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/index";
+    }
+
 }
