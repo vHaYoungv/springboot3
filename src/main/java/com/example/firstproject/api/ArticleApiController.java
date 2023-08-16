fd2d906bb529ef1a61b1ad1e6f1624d3da049d2e
HaYoung Jung <summershine636@gmail.com>
2023-08-16 16:05:20 +0900

    refactor: Article 도메인 서비스-컨트롤러 분리

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -29,11 +29,6 @@ public class ArticleApiController {
     }
 
     //POST
-//    @PostMapping("/api/articles")
-//    public Article create(@RequestBody ArticleForm dto) {
-//        return articleService.create(dto);
-//    } 내 예상: 틀림,
-
     @PostMapping("/api/articles")
     public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
         Article created = articleService.create(dto);
@@ -42,61 +37,21 @@ public class ArticleApiController {
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); //판단 정도는 컨트롤러에서 하는 구나
     }
 
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@RequestBody ArticleForm dto, @PathVariable Long id) {
-//        // 1. 엔티티 변환
-//        Article article = dto.toEntity();
-//
-//        // 2. 타깃 조회
-//        Article target = articleRepository.findById(article.getId()).orElse(null);
-//
-//        // 3. 잘못된 요청 처리하기
-//        if (target == null || id != article.getId()) {
-//            // 400, 잘못된 요청 응답
-//            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//
-//        // 4. 업데이트 및 정상응답하기
-//        target.patch(article);
-//        Article updated = articleRepository.save(article);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@RequestBody ArticleForm dto, @PathVariable Long id) {
+        Article updated = articleService.update(id, dto);
+        return (updated != null)?
+                ResponseEntity.status(HttpStatus.OK).body(updated):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 
-//    //PATCH
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@RequestBody ArticleForm dto, @PathVariable Long id) {
-//        // 1. 엔티티 변환
-//        Article article = dto.toEntity();
-//
-//        // 2. 타깃 조회
-//        Article target = articleRepository.findById(article.getId()).orElse(null);
-//
-//        // 3. 잘못된 요청 처리하기
-//        if (target == null || id != article.getId()) {
-//            // 400, 잘못된 요청 응답
-//            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//
-//        // 4. 업데이트 및 정상응답하기
-//        target.patch(article);
-//        Article updated = articleRepository.save(article);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
-//    //DELETE
-//    @DeleteMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> delete(@PathVariable Long id) {
-//        // 1. 대상 찾기
-//        Article target = articleRepository.findById(id).orElse(null);
-//
-//        // 2. 잘못된 요청 처리하기
-//        if (target == null) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//
-//        // 3. 대상 삭제하기
-//        articleRepository.delete(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(null);
-//    }
+
+    //DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article deleted = articleService.delete(id);
+        return (deleted != null)?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build():
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -30,4 +30,38 @@ public class ArticleService {
         }
         return articleRepository.save(article);
     }
+
+    public Article update(Long id, ArticleForm dto) {
+        // 1. 엔티티 변환
+        Article article = dto.toEntity();
+
+        // 2. 타깃 조회
+        Article target = articleRepository.findById(article.getId()).orElse(null);
+
+        // 3. 잘못된 요청 처리하기
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+
+        // 4. 업데이트 및 정상응답하기
+        target.patch(article);
+        Article updated = articleRepository.save(article);
+        return updated;
+    }
+
+    public Article delete(Long id) {
+        // 1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 2. 잘못된 요청 처리하기
+        if (target == null) {
+            return null;
+        }
+
+        // 3. 대상 삭제하기
+        articleRepository.delete(target);
+        return target;
+    }
 }
