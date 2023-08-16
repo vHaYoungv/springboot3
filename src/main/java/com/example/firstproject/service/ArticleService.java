e2a6b5c48a986d19d97c484786674327f75ffc49
HaYoung Jung <summershine636@gmail.com>
2023-08-16 16:18:15 +0900

    feat: createArticles에 트랜잭션 기능 추가

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -6,8 +6,10 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -63,5 +65,20 @@ public class ArticleService {
         // 3. 대상 삭제하기
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // 1. dto 리스트를 엔티티 리스트로 변환하기
+            List<Article> articleList = dtos.stream().map(dto -> dto.toEntity()).collect(Collectors.toList());
+
+        // 2. 엔티티 리스트를 DB에 저장하기
+            articleList.stream().forEach(article -> articleRepository.save(article));
+
+        // 3. 강제로 에러를 발생시키기
+            articleRepository.findById(-1L).orElseThrow(()->new IllegalArgumentException("결제실패!"));
+
+        // 4. 결과 값 반환하기
+            return articleList;
     }
 }
