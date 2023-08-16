462a2ca248aa0824e2a6ea5177d54f0175dad2e6
HaYoung Jung <summershine636@gmail.com>
2023-08-16 21:38:21 +0900

    feat(Comment): 리파지터리 메소드 추가

--- src/main/java/com/example/firstproject/repository/CommentRepository.java
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(value = "SELECT * FROM comment WHERE article_id = :articleId", nativeQuery = true)
     List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByNickname(String nickname);
 }
