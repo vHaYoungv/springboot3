5bcc89697b47a97396a0595e639958a3c726f48c
HaYoung Jung <summershine636@gmail.com>
2023-08-16 21:00:03 +0900

    feat(Comment): 엔티티, 리파지터리 생성

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 자동으로 1씩 증가
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,16 @@
 INSERT INTO article(title, content) VALUES ('가가','11');
 INSERT INTO article(title, content) VALUES ('나나','22');
 INSERT INTO article(title, content) VALUES ('다다','33');
+INSERT INTO article(title, content) VALUES ('당신의 인생 영화는?','댓글 고');
+INSERT INTO article(title, content) VALUES ('당신의 소울 푸드는?','댓글 고고');
+INSERT INTO article(title, content) VALUES ('당신의 취미는?','댓글 고고고');
+
+INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Park', '굿 윌 헌팅' );
+INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Kim', '아이 엠 샘' );
+INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Park', '치킨' );
+INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Kim', '샤브샤브' );
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Park', '조깅' );
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Kim', '유튜브' );
 
 INSERT INTO member(id, email, password) VALUES (1,'hihi@naver.com','1111');
 
